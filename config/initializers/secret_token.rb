2bb0384a6a684d1b2b03543a8a39551ae1e40df3
Frank Pinto <frank.joseph.pinto@gmail.com>
2013-10-09 12:21:16 -0400

    Generate bare rails app. Overwrote Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FitWorkshop::Application.config.secret_key_base = '3d11d98b7c3dadfc439e58641901edd880370d43d297af68cde3de89f1ee23a74a0e41b8f38bfa5fd62f1578286de422a21c357e1c381592dce6ce2fa8696c62'
